c4c51d80971d7dd6daea415ee693bc86ed457fbe
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-27 10:32:04 -0300

    implementado função de limpar os inputs

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
-const mensagem = document.querySelector('.mensagem-error');
+const mensagem = document.querySelector(".mensagem-error");
+const btnCancelar = document.querySelector(".btn-cancelar");
+
+const inputNome = document.querySelector('input[name="nome"]');
+const inputCPF = document.querySelector('input[name="cpf"]');
+const inputTelefone = document.querySelector('input[name="telefone"]');
+const inputCargo = document.querySelector('input[name="cargo"]');
+const inputSenha = document.querySelector('input[name="senha"]');
 
 if (mensagem) {
   setTimeout(() => {
-    mensagem.style.display = 'none';
+    mensagem.style.display = "none";
   }, 5000);
 }
+
+btnCancelar.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputNome.value = "";
+  inputCPF.value = "";
+  inputTelefone.value = "";
+  inputCargo.value = "";
+  inputSenha.value = "";
+});
