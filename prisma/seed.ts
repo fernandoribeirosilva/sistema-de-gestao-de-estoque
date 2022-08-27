41624d9538299474765534141d30e7edf074cae1
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-08-27 09:16:43 -0300

    foi mudado a estrutura do banco

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,20 +4,31 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  const payload = (Date.now() + Math.random()).toString();
-  const token = await bcrypt.hash(payload, 10);
   const senha = await bcrypt.hash("123", 10);
 
   const user = await prisma.usuario.create({
     data: {
       nome: "admin",
-      cargo: "admin",
-      sexo: "M",
-      token,
+      Cargo: {
+        connectOrCreate: {
+          where: {
+            nome: "gerente",
+          },
+          create: {
+            nome: "gerente",
+          },
+        },
+      },
       senha,
-      email: 'admin@admin.com',
+      cpf: "123.222.444-02",
     },
   });
+
+  // await prisma.cargo.create({
+  //   data: {
+  //     nome: "gerente",
+  //   }
+  // })
 };
 
 main();
