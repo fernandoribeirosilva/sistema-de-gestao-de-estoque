887b299546c8b5918a64277505227e213450d3e0
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-02 10:45:16 -0300

    implementado a listagem dos funcionarios

--- src/controller/UsuarioController.ts
+++ src/controller/UsuarioController.ts
@@ -1,23 +1,39 @@
 import { Request, Response } from "express";
+import { ListaFuncionario } from "../services/admin/funcionario/listaTodosOsFuncionarios";
 
 let typeError: "error" | "success" | "";
 let mensagem: string;
 
 class UsuarioController {
-  index(req: Request, res: Response) {
+  async index(req: Request, res: Response) {
     if (mensagem) {
       setTimeout(() => {
         mensagem = "";
       }, 1000);
     }
-
     const { user } = res.locals;
+    const funcionarios = await ListaFuncionario.listaTodos();
+
     res.render("pages/funcionarios", {
       user,
       ativoMenu: "funcionarios",
       typeError,
       mensagem,
       funcionarios,
+    });
+  }
+
+  async atualizar(req: Request, res: Response) {
+    const { user } = res.locals;
+    const { id } = req.params;
+    console.log(id);
+    const funcionario = await ListaFuncionario.buscarPeloId(+id);
+    res.render("pages/atualizar-funcionarios", {
+      user,
+      ativoMenu: "funcionarios",
+      typeError,
+      mensagem,
+      funcionario,
     });
   }
 }
