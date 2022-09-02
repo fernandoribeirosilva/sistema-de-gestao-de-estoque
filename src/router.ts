4a6ffb5211a7381018613599b1c6f3ac0a6a8326
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-02 08:47:34 -0300

    criado rota de listagem dos funcionários

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import AdminController from "./controller/AdminController";
 import HomeController from "./controller/HomeController";
 import LoginController from "./controller/LoginController";
 import { ProdutoController } from "./controller/ProdutoController";
+import { UsuarioController } from "./controller/UsuarioController";
 import { loginRequired } from "./middlewares/loginRequired";
 
 const router = Router();
@@ -17,6 +18,7 @@ router.get(
 );
 router.get("/novo-produto", loginRequired, new AdminController().novoProduto);
 router.get("/produto/:id", new ProdutoController().index);
+router.get("/funcionarios/", new UsuarioController().index);
 
 router.post("/pesquisa", new HomeController().pesquisa);
 router.post(
